e4094a57f240979d726f5d7b5162fadbc9fbec14
Emrys-Hong <hongpengfei.emrys@gmail.com>
2021-08-11 18:30:36 +0800

    Update featuers
    1. filter by conference
    2. find which year is better

--- main.py
+++ main.py
@@ -29,16 +29,40 @@ def main(
     st.header("ReSearch: Find NLP conference papers easily")
 
     query = st.text_input("What paper are you looking for?", value=query)
-    min_year = st.number_input("Earliest publication year?", value=2018)
+    filter_year = st.sidebar.slider("Filter by Year", 2000, 2021, (2019, 2021), 1)
+    min_year, max_year = filter_year
+    filter_conference = st.multiselect("Filter by Conference",
+            ['None', 'NLP', 'ACL,EMNLP,NAACL', 'CV', 'ML', 'ACL', 'EMNLP', 'NAACL'],
+            default='ACL,EMNLP,NAACL'
+            )
+    conference_list = []
+    if 'None' not in filter_conference: 
+        if 'NLP' in filter_conference:
+            conference_list += ['ACL', 'EMNLP', 'EACL', 'COLING', 'IJNLP', 'NAACL', 'IJCAI', 'AAAI', 'LREC', 'SEMEVAL', 'SIGDIAL']
+        if 'ACL,EMNLP,NAACL' in filter_conference:
+            conference_list += ['ACL', 'EMNLP', 'NAACL']
+        if 'CV' in filter_conference:
+            conference_list += ['CVPR', 'ICCV', 'ECCV']
+        if 'ML' in filter_conference:
+            conference_list += ['ICLR', 'NIPS', 'ICML']
+    else:
+        conference_list = ['ICLR','NIPS', 'ICML', 'AAAI' ,'IJCAI' ,'ACL', 'EMNLP' ,'NAACL' ,'COLING' ,'LREC' ,'EACL' ,'SEMEVAL' ,'SIGDIAL' ,'IJCNLP' ,'INTERSPEECH' ,'ICASSP' ,'CVPR' ,'ICCV' ,'ECCV']
+
+
+
     searcher = pickle.loads(cache_searcher(path_data))
 
     results: List[str] = searcher.run(query)
-    results = [text for text in results if int(text[1:].split()[0]) >= min_year]
     duration = round(time.time() - start, 3)
     st.write(f"About {len(results)} results ({duration} seconds)")
 
-    for r in results[:max_results]:
-        st.markdown(r)
+    num_result = 0
+    for r in results:
+        if min_year <= int(r[1:].split()[0]) <= max_year:
+            if r.split()[1].split(']')[0].upper() in conference_list:
+                st.markdown(r)
+                num_result += 1
+        if num_result >= max_results: break
 
 
 if __name__ == "__main__":
